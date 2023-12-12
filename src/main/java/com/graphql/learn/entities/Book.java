eea9879a777e1840af70f6b3f1af761e8aa51b58
pc <krishradadiya548@gmail.com>
2023-12-12 11:48:16 +0530

    Identity type change in Entity

--- src/main/java/com/graphql/learn/entities/Book.java
+++ src/main/java/com/graphql/learn/entities/Book.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String description;
